da4c21fd4f8571540ee39889921c050c72c6e8f6
alemarco <alessiomar1996@gmail.com>
2025-06-04 11:18:12 +0200

    AM - Some work.

--- public/script.js
+++ public/script.js
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
     function showError(message) {
-        error_div.textContent = message;
+        error_div.value = message;
         error_div.classList.remove('hidden');
     }
 
@@ -27,8 +27,8 @@ document.addEventListener('DOMContentLoaded', () => {
         console.log(q1);
         console.log(q2);
 
-        const doc_id = document.getElementById("add_doc_id").textContent;
-        const doc_text = document.getElementById("add_doc_text").textContent;
+        const doc_id = document.getElementById("add_doc_id").value;
+        const doc_text = document.getElementById("add_doc_text").value;
         
         console.log(`add_document(): ${doc_id} = ${doc_text}`);
 
@@ -53,7 +53,7 @@ document.addEventListener('DOMContentLoaded', () => {
             console.log(`\tAdd Document ${doc_id} Performed Successfully!`);
 
             const add_doc_result = document.getElementById("add_doc_result");
-            add_doc_result.textContent = `Add Document ${doc_id} Performed Successfully!`;
+            add_doc_result.value = `Add Document ${doc_id} Performed Successfully!`;
         } else {
             console.log(`\tError: ${data.error || `Unknown error`}`);
             showError(data.error || 'Unknown error');
@@ -65,7 +65,7 @@ document.addEventListener('DOMContentLoaded', () => {
     async function get_document() {
         error_div.classList.add('hidden');
 
-        const doc_id = document.getElementById("get_doc_id").textContent;
+        const doc_id = document.getElementById("get_doc_id").value;
 
         console.log(`get_document(): ${doc_id}`);
 
@@ -89,7 +89,7 @@ document.addEventListener('DOMContentLoaded', () => {
             console.log(`\tGet Document ${doc_id} Performed Successfully!`);
 
             const get_doc_text = document.getElementById("get_doc_text");
-            get_doc_text.textContent = data.doc_text;
+            get_doc_text.value = data.doc_text;
         } else {
             console.log(`\tError: ${data.error || `Unknown error`}`);
             showError(data.error || 'Unknown error');
@@ -105,10 +105,10 @@ document.addEventListener('DOMContentLoaded', () => {
         const flag2 = document.getElementById("chb_search_term_2").checked;
         const flag3 = document.getElementById("chb_search_term_3").checked;
         const flag4 = document.getElementById("chb_search_term_4").checked;
-        const term1 = document.getElementById("text_search_term_1").textContent;
-        const term2 = document.getElementById("text_search_term_2").textContent;
-        const term3 = document.getElementById("text_search_term_3").textContent;
-        const term4 = document.getElementById("text_search_term_4").textContent;
+        const term1 = document.getElementById("text_search_term_1").value;
+        const term2 = document.getElementById("text_search_term_2").value;
+        const term3 = document.getElementById("text_search_term_3").value;
+        const term4 = document.getElementById("text_search_term_4").value;
 
         let terms = "";
         if (flag1) { terms = `${terms};${term1}`; }
@@ -153,24 +153,24 @@ document.addEventListener('DOMContentLoaded', () => {
             const score_result_5 = document.getElementById('score_result_5');
 
             if (data.num_results >= 1) {
-                id_result_1.textContent = data.docs[0][0];
-                score_result_1.textContent = data.docs[0][1];
+                id_result_1.value = data.docs[0][0];
+                score_result_1.value = data.docs[0][1];
             }
             if (data.num_results >= 2) {
-                id_result_2.textContent = data.docs[1][0];
-                score_result_2.textContent = data.docs[1][1];
+                id_result_2.value = data.docs[1][0];
+                score_result_2.value = data.docs[1][1];
             }
             if (data.num_results >= 3) {
-                id_result_3.textContent = data.docs[2][0];
-                score_result_3.textContent = data.docs[2][1];
+                id_result_3.value = data.docs[2][0];
+                score_result_3.value = data.docs[2][1];
             }
             if (data.num_results >= 4) {
-                id_result_4.textContent = data.docs[3][0];
-                score_result_4.textContent = data.docs[3][1];
+                id_result_4.value = data.docs[3][0];
+                score_result_4.value = data.docs[3][1];
             }
             if (data.num_results >= 5) {
-                id_result_5.textContent = data.docs[4][0];
-                score_result_5.textContent = data.docs[4][1];
+                id_result_5.value = data.docs[4][0];
+                score_result_5.value = data.docs[4][1];
             }
         } else {
             console.log(`\tError: ${data.error || `Unknown error`}`);
